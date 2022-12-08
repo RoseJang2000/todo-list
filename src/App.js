0180039c72b8ac525acd914492bb62dd290cacf3
Rose Jang <jangmi749@gmail.com>
2022-12-08 17:50:13 +0900

    Todo 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-// import './App.scss';
 import React, { useState, useEffect } from 'react';
 import { IoMdAdd } from 'react-icons/io';
 import ListItem from './components/ListItem';
 import TodoTitle from './components/TodoTitle';
-import { ImBold } from 'react-icons/im';
 
 function App() {
   const [todoText, setTodoText] = useState('');
   const [allTodos, setAllTodos] = useState([]);
 
+  /** 새로운 할 일을 추가할 때 작동시킬 함수 */
   const addTodo = e => {
     e.preventDefault();
 
     const todoItem = {
       id: new Date().getTime(),
       text: todoText,
-      isChecked: false
+      isChecked: false,
+      isEdit: false
     };
 
     if (todoText !== '') {
@@ -25,6 +25,7 @@ function App() {
     }
   }
 
+  /** localStorage에 있는 이전 todo 목록들을 불러와 allTodos에 담아준다 */
   const getAllTodos = () => {
     let stored = JSON.parse(localStorage.getItem('todo'));
 
@@ -33,6 +34,7 @@ function App() {
     }
   }
 
+  /** 체크박스 토글기능 */
   const toggleChecked = id => {
     let updatedTodos = allTodos.map(todo => {
       if (todo.id === id) {
@@ -44,9 +46,28 @@ function App() {
     setAllTodos(updatedTodos);
   }
 
+  /** todo 아이템 삭제 */
   const deleteTodo = id => {
     const filteredTodo = allTodos.filter(todo => todo.id !== id);
     setAllTodos(filteredTodo);
+  }
+
+  const toggleEdit = id => {
+    const editTodos = allTodos.map(todo => {
+      if (todo.id === id) todo.isEdit = !todo.isEdit;
+      return todo;
+    })
+
+    setAllTodos(editTodos);
+  }
+
+  const onEditTodo = (e, id) => {
+    const editTodo = allTodos.map(todo => {
+      if (todo.id === id) todo.text = e.target.value;
+      return todo;
+    })
+
+    setAllTodos(editTodo);
   }
 
   useEffect(() => {
@@ -61,7 +82,7 @@ function App() {
     <main className='App'>
       <TodoTitle len={allTodos.length}/>
       <div className='App_todo'>
-        <form className='App_input_wrapper' onSubmit={addTodo}>
+        <form className='App_input_wrapper' onSubmit={e => addTodo(e)}>
           <input 
             type='text' 
             className='App_input' 
@@ -78,10 +99,11 @@ function App() {
             allTodos.map(todo => (
               <ListItem
                 key={todo.id}
-                text={todo.text}
-                isChecked={todo.isChecked}
-                toggleChecked={() => toggleChecked(todo.id)}
-                deleteTodo={() => deleteTodo(todo.id)} />
+                todo={todo}
+                toggleChecked={toggleChecked}
+                deleteTodo={deleteTodo}
+                toggleEdit={toggleEdit}
+                onEditTodo={onEditTodo} />
             ))
           }
           {
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,36 @@
 import './ListItem.css'
 import { AiOutlineCheck } from 'react-icons/ai';
+import { FaEdit } from 'react-icons/fa';
 import { ImBin } from 'react-icons/im' ;
 
 
-const ListItem = ({ text, deleteTodo, isChecked, toggleChecked }) => {
+const ListItem = ({ todo, deleteTodo, toggleChecked, toggleEdit, onEditTodo }) => {
+    const { id, text, isChecked, isEdit } = todo;
+
     return (
         <div className="todo_list">
-            <div className={isChecked ? 'todo_checked' : 'todo_unchecked'} onClick={toggleChecked}>
+            <div className={isChecked ? 'todo_checked' : 'todo_unchecked'} onClick={() => toggleChecked(id)}>
                 {isChecked && <AiOutlineCheck />}
             </div>
-            <div className={isChecked ? 'todo_text_checked' : 'todo_text'}>
+            {isEdit ? 
+                <input  
+                    className={'edit_input'}
+                    placeholder={text}
+                    value={text}
+                    onChange={e => onEditTodo(e, id)}
+                    onKeyPress={e => {
+                        if (e.key === 'Enter') toggleEdit(id);
+                    }}
+                />
+                :
+                <div className={isChecked ? 'todo_text_checked' : 'todo_text'}>
                 {text}
+                </div>
+            }
+            <div className='todo_edit' onClick={() => toggleEdit(id)}>
+                <FaEdit />
             </div>
-            <div className='toto_delete' onClick={deleteTodo}>
+            <div className='todo_delete' onClick={() => deleteTodo(id)}>
                 <ImBin />
             </div>
         </div>
