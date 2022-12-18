2b164cfad77707d9735348edb3c4e506dcace63c
Rose Jang <jangmi749@gmail.com>
2022-12-18 23:00:29 +0900

    Todo count 기능 수정

--- src/App.js
+++ src/App.js
@@ -7,23 +7,24 @@ import TodoTitle from './components/TodoTitle';
 function App() {
   const [todoText, setTodoText] = useState('');
   const [allTodos, setAllTodos] = useState([]);
+  const [count, setCount] = useState(0);
 
   /** 새로운 할 일을 추가할 때 작동시킬 함수 */
-  const addTodo = e => {
+  const addTodo = (e) => {
     e.preventDefault();
 
     const todoItem = {
       id: new Date().getTime(),
       text: todoText,
       isChecked: false,
-      isEdit: false
+      isEdit: false,
     };
 
     if (todoText !== '') {
       setAllTodos([...allTodos, todoItem]);
       setTodoText('');
     }
-  }
+  };
 
   /** localStorage에 있는 이전 todo 목록들을 불러와 allTodos에 담아준다 */
   const getAllTodos = () => {
@@ -32,87 +33,85 @@ function App() {
     if (stored) {
       setAllTodos(stored);
     }
-  }
+  };
 
   /** 체크박스 토글기능 */
-  const toggleChecked = id => {
-    let updatedTodos = allTodos.map(todo => {
+  const toggleChecked = (id) => {
+    let updatedTodos = allTodos.map((todo) => {
       if (todo.id === id) {
         todo.isChecked = !todo.isChecked;
       }
       return todo;
-    })
+    });
 
     setAllTodos(updatedTodos);
-  }
+  };
 
   /** todo 아이템 삭제 */
-  const deleteTodo = id => {
-    const filteredTodo = allTodos.filter(todo => todo.id !== id);
+  const deleteTodo = (id) => {
+    const filteredTodo = allTodos.filter((todo) => todo.id !== id);
     setAllTodos(filteredTodo);
-  }
+  };
 
-  const toggleEdit = id => {
-    const editTodos = allTodos.map(todo => {
+  const toggleEdit = (id) => {
+    const editTodos = allTodos.map((todo) => {
       if (todo.id === id) todo.isEdit = !todo.isEdit;
       return todo;
-    })
+    });
 
     setAllTodos(editTodos);
-  }
+  };
 
   const onEditTodo = (e, id) => {
-    const editTodo = allTodos.map(todo => {
+    const editTodo = allTodos.map((todo) => {
       if (todo.id === id) todo.text = e.target.value;
       return todo;
-    })
+    });
 
     setAllTodos(editTodo);
-  }
+  };
 
   useEffect(() => {
-    getAllTodos()
-  }, [])
+    getAllTodos();
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('todo', JSON.stringify(allTodos));
   }, [allTodos]);
 
+  useEffect(() => {
+    setCount(allTodos.length - allTodos.filter((todo) => todo.isChecked).length);
+  }, [allTodos]);
+
   return (
-    <main className='App'>
-      <TodoTitle len={allTodos.length}/>
-      <div className='App_todo'>
-        <form className='App_input_wrapper' onSubmit={e => addTodo(e)}>
-          <input 
-            type='text' 
-            className='App_input' 
+    <main className="App">
+      <TodoTitle count={count} />
+      <div className="App_todo">
+        <form className="App_input_wrapper" onSubmit={(e) => addTodo(e)}>
+          <input
+            type="text"
+            className="App_input"
             value={todoText}
-            placeholder='Create a new todo...'
-            onChange={e => setTodoText(e.target.value)} />
-          <div className='App_input_button' onClick={e => addTodo(e)}>
+            placeholder="Create a new todo..."
+            onChange={(e) => setTodoText(e.target.value)}
+          />
+          <div className="App_input_button" onClick={(e) => addTodo(e)}>
             <IoMdAdd size={24} />
           </div>
         </form>
 
-        <div className='App_todo_list'>
-          {
-            allTodos.map(todo => (
-              <ListItem
-                key={todo.id}
-                todo={todo}
-                toggleChecked={toggleChecked}
-                deleteTodo={deleteTodo}
-                toggleEdit={toggleEdit}
-                onEditTodo={onEditTodo} />
-            ))
-          }
-          {
-            allTodos.length === 0 && (
-              <p className='empty'>
-                There are no todos
-              </p>
-            )
-          }
+        <div className="App_todo_list">
+          {allTodos.map((todo) => (
+            <ListItem
+              key={todo.id}
+              todo={todo}
+              toggleChecked={toggleChecked}
+              deleteTodo={deleteTodo}
+              toggleEdit={toggleEdit}
+              onEditTodo={onEditTodo}
+            />
+          ))}
+          {allTodos.length === 0 && <p className="empty">There are no todos</p>}
         </div>
       </div>
     </main>
--- src/components/TodoTitle.js
+++ src/components/TodoTitle.js
@@ -1,12 +1,12 @@
 import './TodoTitle.css';
-import './TodoTitle.scss'
+import './TodoTitle.scss';
 
-const TodoTitle = ({len}) => {
-    return (
-        <div className='title_container'>
-            <p className='title_text'>Tasks to Do ({len})</p>
-        </div>
-    );
-}
+const TodoTitle = ({ count }) => {
+  return (
+    <div className="title_container">
+      <p className="title_text">Tasks to Do : {count}</p>
+    </div>
+  );
+};
 
-export default TodoTitle
+export default TodoTitle;
